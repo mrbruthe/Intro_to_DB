d5b2265daa50ba11dbad87c4d1da643c41c5ad8a
MrBruth <133936352+mrbruthe@users.noreply.github.com>
2025-10-19 23:56:47 +0100

    Simplify query to list tables in database

--- task_3.sql
+++ task_3.sql
@@ -1,8 +1,6 @@
 -- task_3.sql
--- This script lists all tables in the alx_book_store database
+-- Lists all tables in the alx_book_store database
 
 USE alx_book_store;
 
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store';
+SHOW TABLES;
