7d61846f9b4585cada73ec1c3f0261a65fccdd56
MrBruth <133936352+mrbruthe@users.noreply.github.com>
2025-10-19 23:58:52 +0100

    Create task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,10 @@
+-- task_6.sql
+-- Inserts multiple customer records into the customers table
+
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
