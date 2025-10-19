b211ce0b73cccaa9f48a4e90eecb559c175397e9
MrBruth <133936352+mrbruthe@users.noreply.github.com>
2025-10-20 00:08:18 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,6 @@
--- task_4.sql
--- Prints the full description of the table books from the alx_book_store database
 
-USE alx_book_store;
 
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+WHERE TABLE_NAME = 'books'
+AND TABLE_SCHEMA = 'alx_book_store';
