42cbd0516d6b9ae05a88c49756477378af176b3e
MrBruth <133936352+mrbruthe@users.noreply.github.com>
2025-10-19 23:55:12 +0100

    Modify SQL script to specify database schema
    Updated the script to explicitly use the 'alx_book_store' schema.

--- task_3.sql
+++ task_3.sql
@@ -1,7 +1,8 @@
 -- task_3.sql
--- Lists all tables in the given database (alx_book_store)
--- without using SHOW or SELECT *
+-- This script lists all tables in the alx_book_store database
+
+USE alx_book_store;
 
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = DATABASE();
+WHERE TABLE_SCHEMA = 'alx_book_store';
